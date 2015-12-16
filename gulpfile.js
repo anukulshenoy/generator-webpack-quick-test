9c7e2e164260c230ae8d1f29fce02f69de6c76b1
sohamkamani <sohamkamani@gmail.com>
2015-12-16 13:27:35 +0530

    removed gulp

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require("gulp");
-var gutil = require("gulp-util");
-var shell = require('gulp-shell');
-
-gulp.task('webpack', shell.task(['./node_modules/.bin/webpack']));
-gulp.task("webpack-dev-server", shell.task(['./node_modules/.bin/webpack-dev-server --progress --colors','open http://localhost:8080/webpack-dev-server/']));
-
-gulp.task('default', ['webpack-dev-server']);
